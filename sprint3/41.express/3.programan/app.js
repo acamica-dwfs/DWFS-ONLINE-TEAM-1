174eee58b50ee4be8d813b7cc4e61b42aa1c9c88
yulian.zapata <yulian.zapata@bimbau.co>
2021-06-03 20:46:05 -0500

    fix

--- sprint3/41.express/3.programan/app.js
+++ sprint3/41.express/3.programan/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const {validar, validarAdmin} = require("./middlewares/autenticacion")
 
 const indexRouter = require('./routes');
 const phoneRouter = require('./routes/telefonos');
 
 app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/telefonos', phoneRouter);
+app.use('/', validar, indexRouter);
+app.use('/telefonos', validarAdmin, phoneRouter);
 
 app.listen(3000, () => {
     console.log("El servidor está inicializado en el puerto 3000");
---
+++ sprint3/41.express/3.programan/middlewares/autenticacion.js
@@ -0,0 +1,13 @@
+
+const   validar = (req, res, next) => {
+    //
+    next()
+}
+
+const   validarAdmin = (req, res, next) => {
+    //
+    next()
+}
+
+module.exports = validar;
+module.exports = validarAdmin;
